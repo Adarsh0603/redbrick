8266e49011e3db6c98ae544f1cb6f5cb1e598c76
hrishikeshrathore <hrishikeshrathore7@gmail.com>
2021-11-28 16:14:37 +0530

    form added

--- redbrick.js
+++ redbrick.js
@@ -3,14 +3,14 @@ const close = document.querySelector(".close");
 const burger = document.querySelector(".burger");
 const navItems = document.querySelector(".nav-items");
 
-// const openModal = document.querySelector(".open-modal");
-// openModal.addEventListener("click", () => {
-//   modal.classList.toggle("is--modal-active");
-// });
+const openModal = document.querySelector(".open-modal");
+openModal.addEventListener("click", () => {
+  modal.classList.toggle("is-modal-active");
+});
 
-// close.addEventListener("click", () => {
-//   modal.classList.toggle("is-modal-active");
-// });
+close.addEventListener("click", () => {
+  modal.classList.toggle("is-modal-active");
+});
 
 function openNav() {
   burger.classList.toggle("change");
