9c182e7374490307ae7964140a4bddf05dac395a
Adarsh0603 <adarshverma0603@gmail.com>
2021-11-28 15:24:09 +0530

    Added responsive navbar

---
+++ redbrick.js
@@ -0,0 +1,18 @@
+const modal = document.querySelector(".modal-bg");
+const close = document.querySelector(".close");
+const burger = document.querySelector(".burger");
+const navItems = document.querySelector(".nav-items");
+
+// const openModal = document.querySelector(".open-modal");
+// openModal.addEventListener("click", () => {
+//   modal.classList.toggle("is--modal-active");
+// });
+
+// close.addEventListener("click", () => {
+//   modal.classList.toggle("is-modal-active");
+// });
+
+function openNav() {
+  burger.classList.toggle("change");
+  navItems.classList.toggle("nav-open");
+}
